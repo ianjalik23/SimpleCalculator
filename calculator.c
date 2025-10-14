98e27e679e39eaedd91bfedff5ac457e099c3c7c
Anjali Kumari <ianjalix23@gmail.com>
2025-10-14 19:47:51 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -1,34 +1,34 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,sum,diff,prod,quo;
+    float a,b,sum,diff,prod,quo;
     char ch;
     printf("Enter first number: ");
-    scanf("%d",&a);
+    scanf("%f",&a);
     printf("Enter second number: ");
-    scanf("%d",&b);
+    scanf("%f",&b);
     printf("Enter your desired operation(+,-,*,/): ");
     scanf(" %c",&ch);
     switch(ch)
     {
         case'+':
         sum=a+b;
-        printf("sum of %d + %d=%d\n",a,b,sum);
+        printf("sum of %f + %f = %f\n",a,b,sum);
         break;
 
         case'-':
         diff=a-b;
-        printf("difference of %d - %d=%d\n",a,b,diff);
+        printf("difference of %f - %f = %f\n",a,b,diff);
         break;
 
         case'*':
         prod=a*b;
-         printf("product of %d * %d=%d\n",a,b,prod);
+         printf("product of %f * %f = %f\n",a,b,prod);
         break;
 
         case'/':
         quo=a/b;
-        printf("quotient of %d / %d=%d\n",a,b,quo);
+        printf("quotient of %f / %f = %f\n",a,b,quo);
         break;
 
         default:
